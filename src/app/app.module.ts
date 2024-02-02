8d03b7bd5f8eae003be8ea9d9710999129807ef0
SalvatorePappagallo <salvatore.pappagallo@intecs.it>
2024-02-02 10:22:42 +0100

    Ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersComponent
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,12 @@
+
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-users',
+    template: `<h2>User</h2>`,
+    standalone: true
+})
+
+export class UsersComponent{
+
+}
